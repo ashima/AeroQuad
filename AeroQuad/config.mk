96db9e332422853e25f8e5b4f2f892f41c97fc1a
Christopher Lee <lee@ashimaresearch.com>
2012-06-02 13:02:26 -0700

    Adding in a Makefile and config.mk
    This branch adds in the makefile into AeroQuad and the config.mk file to configure the USB port.

---
+++ AeroQuad/config.mk
@@ -0,0 +1,7 @@
+INSTALL_DIR = /Applications/Arduino.app/Contents/Resources/Java
+AQ = ../
+PORT = /dev/tty.usbmodemfd321
+UPLOAD_RATE = 115200
+MCU = atmega2560
+AVR_UTILS_PATH = /bin
+#PORT = /dev/tty.usb*
