68b5b5265a7ca8a4e3b7d49b953601bea2743adf
Christopher Lee <lee@ashimaresearch.com>
2012-06-08 23:53:35 -0700

    Reverting 1c97deb..c4cc43e

--- AeroQuad/AeroQuad.ino
+++ AeroQuad/AeroQuad.ino
@@ -1023,7 +1023,7 @@
 #if defined (AeroQuadMega_CHR6DM) || defined (APM_OP_CHR6DM)
   // CHR6DM have it's own kinematics, so, initialize in it's scope
 #else
-  #include "Kinematics_DCM.h"
+  #include "Kinematics_ARG.h"
 #endif
 
 //********************************************************
@@ -1082,7 +1082,7 @@
   #include <HeadingFusionProcessor.h>
 #elif defined (SPARKFUN_9DOF_5883L) || defined (SPARKFUN_5883L_BOB) || defined (AutonavShield_5883L)
   #include <Magnetometer_HMC5883L.h>
-//  #include <HeadingFusionProcessor.h>
+  #include <HeadingFusionProcessor.h>
 #elif defined (COMPASS_CHR6DM)
 #endif
 
@@ -1252,7 +1252,7 @@ void setup() {
     vehicleState |= HEADINGHOLD_ENABLED;
     initializeMagnetometer();
     initializeKinematics(getHdgXY(XAXIS), getHdgXY(YAXIS));
-//    initializeHeadingFusion(getHdgXY(XAXIS), getHdgXY(YAXIS));
+    initializeHeadingFusion(getHdgXY(XAXIS), getHdgXY(YAXIS));
   #else
     initializeKinematics(1.0, 0.0);  // with no compass, DCM matrix initalizes to a heading of 0 degrees
   #endif
@@ -1379,9 +1379,6 @@ void loop () {
                         filteredAccel[XAXIS],
                         filteredAccel[YAXIS],
                         filteredAccel[ZAXIS],
-                         accelOneG,
-                         getHdgXY(XAXIS),
-                         getHdgXY(YAXIS),
                         G_Dt);
 
 
@@ -1448,16 +1445,16 @@ void loop () {
         tenHZpreviousTime = currentTime;
          
         measureMagnetometer(kinematicsAngle[XAXIS], kinematicsAngle[YAXIS]);
-//        calculateHeading(gyroRate[XAXIS],
-//                         gyroRate[YAXIS],
-//                         gyroRate[ZAXIS],
-//                         filteredAccel[XAXIS],
-//                         filteredAccel[YAXIS],
-//                         filteredAccel[ZAXIS],
-//                         accelOneG,
-//                         getHdgXY(XAXIS),
-//                         getHdgXY(YAXIS),
-//                         G_Dt);
+        calculateHeading(gyroRate[XAXIS],
+                         gyroRate[YAXIS],
+                         gyroRate[ZAXIS],
+                         filteredAccel[XAXIS],
+                         filteredAccel[YAXIS],
+                         filteredAccel[ZAXIS],
+                         accelOneG,
+                         getHdgXY(XAXIS),
+                         getHdgXY(YAXIS),
+                         G_Dt);
       #endif
     }
     else if ((currentTime - lowPriorityTenHZpreviousTime) > 100000) {
