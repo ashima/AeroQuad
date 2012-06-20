4df934f9703c7cd4c74461c385a7f052c41665fb
Christopher Lee <lee@ashimaresearch.com>
2012-06-20 09:22:40 -0700

    mod 10=9 forcing logging 16 shorts : 100Hz at 867ms, 10Hz at 286ms

--- AeroQuad/AeroQuad.ino
+++ AeroQuad/AeroQuad.ino
@@ -1585,7 +1585,7 @@ void loop () {
 //          }
 //          LogValueSpace((float)batteryData[0].voltage/100.0);
           for (byte motor = 0; motor < 16; motor++) {
-            LogValueSpace(1L);
+            LogValueSpace((short)1);
           }
         
           LOG_SERIAL.println();
